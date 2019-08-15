6f5e1fdf1f389fe005f3581a2f3ccd1cea68f3f0
Your Name <you@example.com>
2019-08-15 10:20:23 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Time from "./Time/Time";
-import "./styles.css";
+import "./index.css";
 
 class App extends React.Component {
   constructor(props) {

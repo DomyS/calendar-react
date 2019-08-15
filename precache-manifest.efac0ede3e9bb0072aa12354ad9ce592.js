5af105f31e62efb8d7bf0e12f3eaa016bd6319c9
Your Name <you@example.com>
2019-08-15 10:06:59 +0200

    Updates

---
+++ precache-manifest.efac0ede3e9bb0072aa12354ad9ce592.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8156f38682270547ff4ad66dc66786f1",
+    "url": "/calendar-react-lappen/index.html"
+  },
+  {
+    "revision": "21060935e4d73d8f47ad",
+    "url": "/calendar-react-lappen/static/css/main.85146dac.chunk.css"
+  },
+  {
+    "revision": "c0bfc0dc1f86520449b8",
+    "url": "/calendar-react-lappen/static/js/2.94471727.chunk.js"
+  },
+  {
+    "revision": "21060935e4d73d8f47ad",
+    "url": "/calendar-react-lappen/static/js/main.cad9ffe6.chunk.js"
+  },
+  {
+    "revision": "a0f1e4d09446dfdfa7fc",
+    "url": "/calendar-react-lappen/static/js/runtime~main.634d37f0.js"
+  }
+]);
